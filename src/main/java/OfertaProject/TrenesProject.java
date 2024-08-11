430b8b6b86bf5ac3388c61ffacdab68a6a5b18f1
ahm22508 <ahmadamr659@gmail.com>
2024-08-11 06:12:19 +0300

    commit

--- src/main/java/OfertaProject/TrenesProject.java
+++ src/main/java/OfertaProject/TrenesProject.java
@@ -7,15 +7,14 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TrenesProject {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NullPointerException {
         //ask the end to send  the file
         Scanner scanFile = new Scanner(System.in);
         System.out.println("Send the file to us...");
@@ -32,29 +31,83 @@ public class TrenesProject {
             for (int i = 1; i < Num; i++) {
                 text.append(PdfTextExtractor.getTextFromPage(pdfDoc.getPage(i)));
             }
-
             //Create a new sheet into the Excel file to populate it the extracted data.
             Sheet sheet = workbook.createSheet("Trenes");
-
-            Pattern pattern = Pattern.compile("^DRZRW$");
-            Matcher matcher = pattern.matcher(text);
-            Pattern pattern1 = Pattern.compile("^\\d+(\\.\\d+)?%");
+            Pattern pattern1 = Pattern.compile("\\d+(\\.\\d+)?+(?=%)");
             Matcher matcher1 = pattern1.matcher(text);
-
             int i = 0;
+
+
             Row row;
+            Pattern pattern = Pattern.compile("D+(?!TFWP)[A-Z]{4}");
+            Matcher matcher = pattern.matcher(text);
+            int x = 0;
+//            boolean Pointer = false;
+            List<String> Values = new ArrayList<>();
             while (matcher.find()) {
-                row = sheet.createRow(i++);
-                row.createCell(0).setCellValue(matcher.group());
-//                row.createCell(1).setCellValue(matcher1.group());
+                String FinalValue = matcher.group();
+                if (!Values.contains(FinalValue)) {
+//                    Pointer = true;
+                    Values.add(FinalValue);
+                    row = sheet.createRow(x++);
+                    row.createCell(0).setCellValue(FinalValue);
+                    while (matcher1.find()) {
+                        row = sheet.getRow(i++);
+                        row.createCell(1).setCellValue(matcher1.group());
+                        break;
+                    }
+                }
+                else {
+                    row = sheet.getRow(i);
+                    row.createCell(1).setCellValue(matcher1.group());
+                    row.createCell(1).setCellValue("");
+                        break;
+                }
             }
 
-            try (FileOutputStream outputStream = new FileOutputStream("Fichero.xlsx")) {
-                workbook.write(outputStream);
+//                    row = sheet.createRow(x++);
+
+
+//                    for (String value : Values) {
+//                        row.createCell(0).setCellValue(value);
+//                    }
+
+
+//                for (String value : Values) {
+//                    row = sheet.createRow(x++);
+//                    row.createCell(0).setCellValue(value);
+//                }
+
+//                while (matcher1.find()) {
+//
+//                        row = sheet.createRow(i++);
+//                        row.createCell(1).setCellValue(matcher1.group());
+//                        if(Pointer){
+//                            row.getCell(1).setCellValue("");
+//                        }
+//
+//                }
+//                for (String value : Values) {
+//                    row = sheet.getRow(x++);
+//                    row.createCell(0).setCellValue(value);
+//                }
+
+
+
+
+                try (FileOutputStream outputStream = new FileOutputStream("Fichero.xlsx")) {
+                    workbook.write(outputStream);
+                }
             }
             //handle any type of error during code process.
-        } catch (IOException e) {
-            e.getCause();
+        catch(IOException e){
+                e.getCause();
+            }
         }
     }
-}
+
+
+
+
+
+
